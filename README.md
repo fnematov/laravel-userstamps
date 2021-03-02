4c228eaf7afa7ca67a83525ea8571301e38d06e0
fnematov <mr.nematov91@gmail.com>
2021-03-02 09:36:20 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Laravel Userstamps
+
+Laravel Userstamps is a simple Laravel package for your Eloquent Model user specific fields.
+This package automatically inserts/updates an user id on your table on who created, last updated and deleted the record.
+
+## Install
+1. Add repositories in composer.json
+
+```
+composer require fnematov/laravel-userstamps
+```
+## Usage
+
+Update your model's migration and add `created_by`, `updated_by` and `deleted_by` field using the `userstamps()` blueprint macro.
+
+```php
+Schema::create('users', function (Blueprint $table) {
+    $table->userstamps();
+});
+```
+
+Then use `UserstampsTrait` on your model.
+
+``` php
+namespace App;
+
+use Fnematov\Userstamps\UserstampsTrait;
+
+class User extends Model
+{
+    use UserstampsTrait;
+}
+```
+
+## Dropping columns
+
+You can drop auditable columns using `dropUserstamps()` method.
+
+```php
+Schema::create('users', function (Blueprint $table) {
+    $table->dropUserstamps();
+});
+```
+
+And your done!
+
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/UserStampsServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Fnematov\Userstamps;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Blueprint;
+
+
+class UserstampsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Blueprint::macro(
+            'userstamps', function () {
+            $this->unsignedInteger('created_by')->nullable()
+                ->index();
+            $this->unsignedInteger('updated_by')->nullable()
+                ->index();
+            $this->unsignedInteger('deleted_by')->nullable()
+                ->index();
+        }
+        );
+        Blueprint::macro(
+            'addForiegnUserstamps', function () {
+            $this->foreign('created_by')
+                ->references('id')->on('users')->onDelete('cascade');
+            $this->foreign('updated_by')
+                ->references('id')->on('users')->onDelete('cascade');
+            $this->foreign('deleted_by')
+                ->references('id')->on('users')->onDelete('cascade');
+        }
+        );
+        Blueprint::macro(
+            'dropUserstamps', function () {
+            $this->dropColumn(['created_by', 'updated_by', 'deleted_by']);
+        }
+        );
+        Blueprint::macro(
+            'dropForeignUserstamps', function () {
+            $this->dropForeign(['created_by', 'updated_by', 'deleted_by']);
+        }
+        );
+    }
+}
